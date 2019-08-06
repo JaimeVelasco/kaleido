46f74f414e3a83d3022e925bfa6442ad745fcab6
JaimeVelasco <contact@jaimevelas.co>
2019-08-06 13:57:17 -0500

    add redux, create store, test actions and reducers

--- src/App.js
+++ src/App.js
@@ -2,22 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import { useSelector, useDispatch } from "react-redux";
+import { simpleAction } from "./actions/simpleAction";
+
+function App({ data }) {
+  const message = useSelector(state => state.result);
+  const dispatch = useDispatch();
+
+  console.log('message', message)
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{message.result}</p>
+        <button onClick={() => dispatch(simpleAction())}>Test redux action</button>
       </header>
     </div>
   );
---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import result from './simpleReducer';
+
+export default combineReducers({
+ result
+});
